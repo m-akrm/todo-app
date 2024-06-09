920f26283168cca05f4e3f340da05016c61b95e5
m-akrm <mahmoud.akrm@ejust.edu.eg>
2024-06-09 09:59:12 +0300

    implemeting calenderview

--- app/src/main/java/com/example/todo_app/CalenderFragment.kt
+++ app/src/main/java/com/example/todo_app/CalenderFragment.kt
@@ -5,55 +5,101 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.todo_app.databinding.FragmentCalenderBinding
+import com.michalsvec.singlerowcalendar.calendar.CalendarChangesObserver
+import com.michalsvec.singlerowcalendar.calendar.CalendarViewManager
+import com.michalsvec.singlerowcalendar.calendar.SingleRowCalendarAdapter
+import com.michalsvec.singlerowcalendar.selection.CalendarSelectionManager
+import com.michalsvec.singlerowcalendar.utils.DateUtils
+import java.util.Calendar
+import java.util.Date
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [CalenderFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CalenderFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private lateinit var binding:FragmentCalenderBinding
+    private val calendar = Calendar.getInstance()
+    private var currentMonth = 0
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calender, container, false)
+        binding=DataBindingUtil.inflate(layoutInflater,R.layout.fragment_calender, container, false)
+        binding.lifecycleOwner=viewLifecycleOwner
+
+        // set current date to calendar and current month to currentMonth variable
+        calendar.time = Date()
+        currentMonth = calendar[Calendar.MONTH]
+
+
+        // calendar view manager is responsible for our displaying logic
+        val myCalendarViewManager = object :
+            CalendarViewManager {
+            override fun bindDataToCalendarView(
+                holder: SingleRowCalendarAdapter.CalendarViewHolder,
+                date: Date,
+                position: Int,
+                isSelected: Boolean
+            ) {
+                val daytextview=holder.itemView.findViewById<TextView>(R.id.tv_day_calendar_item)
+                val monthtextview=holder.itemView.findViewById<TextView>(R.id.tv_month_calendar_item)
+                val datetextview=holder.itemView.findViewById<TextView>(R.id.tv_date_calendar_item)
+
+                datetextview.text=DateUtils.getDayNumber(date)
+                monthtextview.text=DateUtils.getMonth3LettersName(date)
+                daytextview.text=DateUtils.getDay3LettersName(date)
+
+            }
+
+            override fun setCalendarViewResourceId(
+                position: Int,
+                date: Date,
+                isSelected: Boolean
+            ): Int {
+
+                return if (isSelected) R.layout.selected_calendar_item
+                else R.layout.calendar_item
+
+            }
+        }
+
+
+        // using calendar changes observer we can track changes in calendar
+        val myCalendarChangesObserver = object :
+            CalendarChangesObserver {
+            override fun whenSelectionChanged(isSelected: Boolean, position: Int, date: Date) {
+                // TODO: change recyclerview list with new date
+                if(isSelected){
+                    Toast.makeText(this@CalenderFragment.requireContext(), "${DateUtils.getMonthName(date)}, ${DateUtils.getDayNumber(date)} ", Toast.LENGTH_SHORT).show()
+
+                }
+                super.whenSelectionChanged(isSelected, position, date)
+            }
+        }
+
+        // selection manager is responsible for managing selection
+        val mySelectionManager = object : CalendarSelectionManager {
+            override fun canBeItemSelected(position: Int, date: Date): Boolean {
+                return true
+            }
+        }
+        binding.singleRowCal.apply {
+            calendarViewManager = myCalendarViewManager
+            calendarChangesObserver = myCalendarChangesObserver
+            calendarSelectionManager = mySelectionManager
+            includeCurrentDate=true
+            futureDaysCount = 7
+            initialPositionIndex=5
+            pastDaysCount=7
+            init()
+        }
+        binding.singleRowCal.select(7)
+
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CalenderFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CalenderFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
