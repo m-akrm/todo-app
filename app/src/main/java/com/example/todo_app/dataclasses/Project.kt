3f8a2e502fe05d094689faf5decf90c3f77e3241
m-akrm <mahmoud.akrm@ejust.edu.eg>
2024-06-18 14:11:03 +0300

    add room database dependency

--- app/src/main/java/com/example/todo_app/dataclasses/Project.kt
+++ app/src/main/java/com/example/todo_app/dataclasses/Project.kt
@@ -1,10 +1,12 @@
 package com.example.todo_app.dataclasses
 
 import android.os.Parcelable
+import androidx.room.Entity
 import kotlinx.android.parcel.Parcelize
 
 
 @Parcelize
+@Entity
 data class Project(
     var projectName: String,
     var taskscount: Int,
